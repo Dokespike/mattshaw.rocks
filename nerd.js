55c006e2ef760095d52c3f312d3588431e659935
dokespike <dokespike@gmail.com>
2014-09-20 01:56:10 -0400

    asdf

--- nerd.js
+++ nerd.js
@@ -1,2 +1,22 @@
-var audio = new Audio('strangesr.wav');
+
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+
+var audio = new Audio('strangers.wav');
 audio.play();
