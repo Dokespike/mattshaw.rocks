074848e48c830a91bbaff44941b41682005704e3
Doke <dokespike@gmail.com>
2014-12-16 22:22:22 -0500

    a

---
+++ index.php
@@ -0,0 +1,119 @@
+<!--
+ _________________________________________
+/ Hello, I am a cow.                      \
+|                                         |
+| Stay in school and do what you love     |
+| kids! The code sources used here are    |
+| Free Software so feel free to use, edit,|
+| and distribute my code with or without  |
+| edits. The only request is you allow    |
+| people to do the same.                  |
+ -----------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+
+
+-->
+
+<head>
+<title>Matt Shaw</title>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<link rel="shortcut icon" href="http://sstatic.net/stackoverflow/img/favicon.ico">
+<script src="nerd.js"></script>
+<link rel="stylesheet" type="text/css" href="nerd.css">
+</head>
+
+
+
+
+
+
+<!--Start of the Website-->
+<body onload='location.href="index.html#mission"'>
+<div class="container">
+
+
+
+	
+
+<!-- Header -->	
+<div class="navBar">
+	<table width="100%" height="100%">
+		<tr>
+			<td>
+				<p class="title">&nbsp;&nbsp;Matt Shaw</p>
+			</td>
+			<td>
+				<a href="index.html#mission">
+					<div class="navButton">&nbsp;&nbsp;Mission</div>
+				</a>
+			</td>
+			<td>
+				<a href="index.html#about">
+					<div class="navButton">&nbsp;&nbsp;About</div>
+				</a>
+			</td>
+			<td>
+				<a href="index.html#portfolio">
+					<div class="navButton">&nbsp;&nbsp;Portfolio</div>
+				</a>
+			</td>
+			<td>
+				<a href="index.html#contact">
+					<div class="navButton">&nbsp;&nbsp;Contact</div>
+				</a>
+			</td>
+		</tr>
+	</table>
+</div>
+
+
+
+<!--Content-->
+<p id="mission"></p></br></br></br></br></br>
+
+<?php $yourOS = PHP_OS; ?>
+<?php if ($yourOS == "Linux"): ?>
+	<p class="title">You use GNU/Linux!! Awsome!</h1>
+	<br>
+	<img src="Gnulinux.png">
+<?php else: ?>	
+	<p class="title">Mission</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed auctor odio. Phasellus at tempus lorem. Nullam auctor, dolor eu egestas consequat, nisi augue faucibus elit, a accumsan nulla leo quis erat. Ut eget dignissim purus. Mauris interdum ipsum eu urna ultricies euismod. Cras eget sollicitudin nunc. Duis eu tortor eu odio tincidunt tincidunt vel vitae enim. Ut eleifend, mi non suscipit gravida, leo nulla porta orci, vitae maximus purus lectus quis ipsum. Maecenas finibus sodales sapien. Aenean sit amet diam scelerisque, egestas leo semper, ornare enim. Mauris in laoreet dui. Nunc tempus risus vitae placerat ultrices. Pellentesque vel ornare elit. Phasellus tortor lectus, hendrerit ut efficitur in, eleifend quis ex.</br></br>
+	Integer elementum vitae felis in viverra. Donec pretium mattis turpis. Morbi congue leo vitae massa egestas, vitae fringilla dolor euismod. Sed ut facilisis mauris. Nulla facilisi. Suspendisse iaculis, massa sit amet luctus mollis, metus odio ornare sapien, eu condimentum enim nunc vitae lectus. Duis fringilla vel tellus eget gravida. In non rhoncus sem. Pellentesque in laoreet dolor. Nam finibus sapien ut purus pellentesque, vitae dignissim tellus feugiat. </p><p id="about"></p>
+<?php endif; ?>
+<br><br><br><hr><br>
+
+<p class="title">About</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed auctor odio. Phasellus at tempus lorem. Nullam auctor, dolor eu egestas consequat, nisi augue faucibus elit, a accumsan nulla leo quis erat. Ut eget dignissim purus. Mauris interdum ipsum eu urna ultricies euismod. Cras eget sollicitudin nunc. Duis eu tortor eu odio tincidunt tincidunt vel vitae enim. Ut eleifend, mi non suscipit gravida, leo nulla porta orci, vitae maximus purus lectus quis ipsum. Maecenas finibus sodales sapien. Aenean sit amet diam scelerisque, egestas leo semper, ornare enim. Mauris in laoreet dui. Nunc tempus risus vitae placerat ultrices. Pellentesque vel ornare elit. Phasellus tortor lectus, hendrerit ut efficitur in, eleifend quis ex.</br></br>
+Integer elementum vitae felis in viverra. Donec pretium mattis turpis. Morbi congue leo vitae massa egestas, vitae fringilla dolor euismod. Sed ut facilisis mauris. Nulla facilisi. Suspendisse iaculis, massa sit amet luctus mollis, metus odio ornare sapien, eu condimentum enim nunc vitae lectus. Duis fringilla vel tellus eget gravida. In non rhoncus sem. Pellentesque in laoreet dolor. Nam finibus sapien ut purus pellentesque, vitae dignissim tellus feugiat. </p><p id="portfolio"></p>
+
+<br><br><br><hr><br>
+
+<p class="title">Portfolio</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed auctor odio. Phasellus at tempus lorem. Nullam auctor, dolor eu egestas consequat, nisi augue faucibus elit, a accumsan nulla leo quis erat. Ut eget dignissim purus. Mauris interdum ipsum eu urna ultricies euismod. Cras eget sollicitudin nunc. Duis eu tortor eu odio tincidunt tincidunt vel vitae enim. Ut eleifend, mi non suscipit gravida, leo nulla porta orci, vitae maximus purus lectus quis ipsum. Maecenas finibus sodales sapien. Aenean sit amet diam scelerisque, egestas leo semper, ornare enim. Mauris in laoreet dui. Nunc tempus risus vitae placerat ultrices. Pellentesque vel ornare elit. Phasellus tortor lectus, hendrerit ut efficitur in, eleifend quis ex.</br></br>
+Integer elementum vitae felis in viverra. Donec pretium mattis turpis. Morbi congue leo vitae massa egestas, vitae fringilla dolor euismod. Sed ut facilisis mauris. Nulla facilisi. Suspendisse iaculis, massa sit amet luctus mollis, metus odio ornare sapien, eu condimentum enim nunc vitae lectus. Duis fringilla vel tellus eget gravida. In non rhoncus sem. Pellentesque in laoreet dolor. Nam finibus sapien ut purus pellentesque, vitae dignissim tellus feugiat. </p><p id="contact">
+
+<br><br><br></p><hr><br>
+
+<p class="title">Contact</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed auctor odio. Phasellus at tempus lorem. Nullam auctor, dolor eu egestas consequat, nisi augue faucibus elit, a accumsan nulla leo quis erat. Ut eget dignissim purus. Mauris interdum ipsum eu urna ultricies euismod. Cras eget sollicitudin nunc. Duis eu tortor eu odio tincidunt tincidunt vel vitae enim. Ut eleifend, mi non suscipit gravida, leo nulla porta orci, vitae maximus purus lectus quis ipsum. Maecenas finibus sodales sapien. Aenean sit amet diam scelerisque, egestas leo semper, ornare enim. Mauris in laoreet dui. Nunc tempus risus vitae placerat ultrices. Pellentesque vel ornare elit. Phasellus tortor lectus, hendrerit ut efficitur in, eleifend quis ex.</br></br>
+Integer elementum vitae felis in viverra. Donec pretium mattis turpis. Morbi congue leo vitae massa egestas, vitae fringilla dolor euismod. Sed ut facilisis mauris. Nulla facilisi. Suspendisse iaculis, massa sit amet luctus mollis, metus odio ornare sapien, eu condimentum enim nunc vitae lectus. Duis fringilla vel tellus eget gravida. In non rhoncus sem. Pellentesque in laoreet dolor. Nam finibus sapien ut purus pellentesque, vitae dignissim tellus feugiat. </p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed auctor odio. Phasellus at tempus lorem. Nullam auctor, dolor eu egestas consequat, nisi augue faucibus elit, a accumsan nulla leo quis erat. Ut eget dignissim purus. Mauris interdum ipsum eu urna ultricies euismod. Cras eget sollicitudin nunc. Duis eu tortor eu odio tincidunt tincidunt vel vitae enim. Ut eleifend, mi non suscipit gravida, leo nulla porta orci, vitae maximus purus lectus quis ipsum. Maecenas finibus sodales sapien. Aenean sit amet diam scelerisque, egestas leo semper, ornare enim. Mauris in laoreet dui. Nunc tempus risus vitae placerat ultrices. Pellentesque vel ornare elit. Phasellus tortor lectus, hendrerit ut efficitur in, eleifend quis ex.</br></br>
+Integer elementum vitae felis in viverra. Donec pretium mattis turpis. Morbi congue leo vitae massa egestas, vitae fringilla dolor euismod. Sed ut facilisis mauris. Nulla facilisi. Suspendisse iaculis, massa sit amet luctus mollis, metus odio ornare sapien, eu condimentum enim nunc vitae lectus. Duis fringilla vel tellus eget gravida. In non rhoncus sem. Pellentesque in laoreet dolor. Nam finibus sapien ut purus pellentesque, vitae dignissim tellus feugiat. </p>
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+</body>
